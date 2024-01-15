2b6cc763bd57033c931fcefc3e30a005e7556c15
VictorGarciaGtz <victor.garciagutierrez.itcm@gmail.com>
2024-01-14 18:10:55 -0600

    Se realiza la incorporacion del paquete FluentValidation, se crean clases para las validaciones de Beer.

---
+++ CursoBackend/Validators/BeerUpdateValidator.cs
@@ -0,0 +1,18 @@
+﻿using CursoBackend.DTOs;
+using FluentValidation;
+
+namespace CursoBackend.Validators
+{
+    public class BeerUpdateValidator: AbstractValidator<BeerUpdateDto>
+    {
+        public BeerUpdateValidator() {
+            RuleFor(x => x.Id).NotNull().WithMessage("El id es obligatorio");
+            RuleFor(x => x.Name).NotEmpty().WithMessage("El nombre es obligatorio.");
+            RuleFor(x => x.Name).Length(2, 20).WithMessage("El nombre debe estar entre 2 a 20 caracteres.");
+            RuleFor(x => x.BrandID).NotNull().WithMessage("La marca es obligatoria.");
+            RuleFor(x => x.BrandID).GreaterThan(0).WithMessage("La marca debe ser mayor a 0.");
+            RuleFor(x => x.Alcohol).GreaterThan(0).WithMessage("El {PropertyName} debe ser mayor a 0.");
+
+        }
+    }
+}
